fc6759bd0e9a4cf7654f6298460e90590f30bbc8
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-05-21 13:03:45 +0900

    chore:프로젝트 초기 설정

---
+++ cake-quake-back/src/main/java/com/cakequake/cakequakeback/CakeQuakeBackApplication.java
@@ -0,0 +1,13 @@
+package com.cakequake.cakequakeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeQuakeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeQuakeBackApplication.class, args);
+	}
+
+}
