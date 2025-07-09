e4da515d1f05bb7a580eb2b1ba3c4d5494c42482
Minsol Kim <yhe071624@gmail.com>
2025-07-09 22:49:23 +0900

    fix:chatting fix

--- cake-quake-back/src/main/java/com/cakequake/cakequakeback/chatting/controller/ChatMessageController.java
+++ cake-quake-back/src/main/java/com/cakequake/cakequakeback/chatting/controller/ChatMessageController.java
@@ -2,11 +2,14 @@ package com.cakequake.cakequakeback.chatting.controller;
 
 import com.cakequake.cakequakeback.chatting.dto.ChatMessageDto;
 import com.cakequake.cakequakeback.chatting.service.ChatService;
+import com.cakequake.cakequakeback.member.entities.Member;
 import com.cakequake.cakequakeback.security.domain.CustomUserDetails;
+import com.cakequake.cakequakeback.security.service.AuthenticatedUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
@@ -16,15 +19,23 @@ import org.springframework.stereotype.Controller;
 public class ChatMessageController {
 
     private final ChatService chatService;
+    private final AuthenticatedUserService authenticatedUserService;
 
 
     @MessageMapping("/chat/{roomKey}")
 
     public void sendChatMessage(@DestinationVariable String roomKey, ChatMessageDto messageDto) {
-        // userDetails 대신 messageDto에서 보낸 사람의 UID를 로그에 사용
-        log.info("ChatMessageController: 메시지 수신 요청 - roomKey={}, sender={}", roomKey, messageDto.getSenderUid());
+        // 👉 현재 로그인한 사용자 정보 가져오기 (SecurityContextHolder 접근 X)
+        Member currentMember = authenticatedUserService.getCurrentMember();
+        Long senderUid = currentMember.getUid();
+        String senderUsername = currentMember.getUserId();  // Member에 username 필드가 있다고 가정
 
-        // ⭐ chatService 호출 시 userDetails 매개변수 제거
+        // ❗ 프론트에서 보낸 sender 정보 무시하고 강제 세팅
+        messageDto.setSenderUid(senderUid);
+        messageDto.setSenderUsername(senderUsername);
+
+        log.info("ChatMessageController: 메시지 수신 요청 - roomKey={}, sender={}", roomKey, senderUid);
+
         chatService.processAndBroadcastChatMessage(roomKey, messageDto);
 
         log.info("ChatMessageController: 메시지 처리 요청 완료 (서비스로 위임)");
